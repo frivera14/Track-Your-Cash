98e48324c2fb4915cc9743057d4871d217d349d0
unknown <kikridu@gmail.com>
2022-06-02 16:13:20 -0700

    server errors

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/Track-Your-Cash";
 
 const app = express();
 
@@ -16,8 +16,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI || 'mongodb://localhost:27017/track-your-cash', {
+mongoose.connect(MONGODB_URI,  {
   useNewUrlParser: true,
+  useFindAndModify: false,
   useUnifiedTopology: true
 });
 
